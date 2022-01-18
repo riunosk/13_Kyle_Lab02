ede5e3f29ae62c1d649a7f1fd520bc2c3b3234e1
riunosk <riunosk@outlook.com>
2022-01-18 14:27:46 +0800

    player 1 win condition (red)

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -14,7 +14,9 @@ public class Controller : MonoBehaviour
     public GameObject Cube7;
     public GameObject Cube8;
     public GameObject Cube9;
+    public static int Winner;
 
+    private bool GameRunning = true;
     private int Player = 1;
     private Color TurnColor = Color.red;
     // Start is called before the first frame update
@@ -34,7 +36,12 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(Player);
+        RedWin();
+        print(GameRunning);
+        if (GameRunning == false)
+		{
+            SceneManager.LoadScene("End");
+		}
         if (Player == 1)
 		{
             TurnColor = Color.red;
@@ -43,47 +50,47 @@ public class Controller : MonoBehaviour
 		{
             TurnColor = Color.green;
 		}
-        if (Input.GetKeyDown(KeyCode.Q) && Cube1.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.Q) && Cube1.GetComponent<MeshRenderer>().material.color == Color.white)
 		{
             Cube1.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.W) && Cube2.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.W) && Cube2.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube2.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.E) && Cube3.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.E) && Cube3.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube3.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.A) && Cube4.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.A) && Cube4.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube4.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.S) && Cube5.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.S) && Cube5.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube5.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.D) && Cube6.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.D) && Cube6.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube6.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.Z) && Cube7.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.Z) && Cube7.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube7.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.X) && Cube8.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.X) && Cube8.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube8.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
         }
-        if (Input.GetKeyDown(KeyCode.C) && Cube9.GetComponent<MeshRenderer>().material.color == Color.white)
+        if (GameRunning == true && Input.GetKeyDown(KeyCode.C) && Cube9.GetComponent<MeshRenderer>().material.color == Color.white)
         {
             Cube9.GetComponent<MeshRenderer>().material.color = TurnColor;
             NextTurn();
@@ -100,10 +107,45 @@ public class Controller : MonoBehaviour
 	}
     private void RedWin()
 	{
-        if (Cube1.GetComponent<MeshRenderer>().material.color == Color.red && Cube2.GetComponent<MeshRenderer>().material.color == Color.red)
+        if (Cube1.GetComponent<MeshRenderer>().material.color == Color.red && Cube2.GetComponent<MeshRenderer>().material.color == Color.red && Cube3.GetComponent<MeshRenderer>().material.color == Color.red)
 		{
-
+            Winner = 1;
+            GameRunning = false;
 		}
-
+        if (Cube4.GetComponent<MeshRenderer>().material.color == Color.red && Cube5.GetComponent<MeshRenderer>().material.color == Color.red && Cube6.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube7.GetComponent<MeshRenderer>().material.color == Color.red && Cube8.GetComponent<MeshRenderer>().material.color == Color.red && Cube9.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube1.GetComponent<MeshRenderer>().material.color == Color.red && Cube4.GetComponent<MeshRenderer>().material.color == Color.red && Cube7.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube2.GetComponent<MeshRenderer>().material.color == Color.red && Cube5.GetComponent<MeshRenderer>().material.color == Color.red && Cube8.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube3.GetComponent<MeshRenderer>().material.color == Color.red && Cube6.GetComponent<MeshRenderer>().material.color == Color.red && Cube9.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube1.GetComponent<MeshRenderer>().material.color == Color.red && Cube5.GetComponent<MeshRenderer>().material.color == Color.red && Cube9.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
+        if (Cube7.GetComponent<MeshRenderer>().material.color == Color.red && Cube5.GetComponent<MeshRenderer>().material.color == Color.red && Cube3.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            Winner = 1;
+            GameRunning = false;
+        }
     }
 }
